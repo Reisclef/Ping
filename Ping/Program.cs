ffe3a80ca843f5716aa55b04b1e8a91e6880c52d
Resiclef <watch4thesql@outlook.com>
2016-11-03 17:26:31 +0000

    Created default args, and added repetition

--- Ping/Program.cs
+++ Ping/Program.cs
@@ -5,39 +5,77 @@ using System.IO;
 
 namespace Ping {
     class Program {
+        static string location = "https://google.com";
+        static string path = Directory.GetCurrentDirectory() + "\\log.log";
+        static int repetitions = 1;
         static void Main(string[] args) {
-            if (args.Length != 2) {
-                Console.WriteLine("Usage: URL_to_ping log_file");
+            //Based on the number of arguments, set the variables from default
+            switch (args.Length) {
+                case 0:
+                    Console.WriteLine("Usage: URL_to_ping log_file repetitions");
+                    break;
+                case 1:
+                    Console.WriteLine("Usage: URL_to_ping log_file repetitions");
+                    location = args[0];
+                    break;
+                case 2:
+                    Console.WriteLine("Usage: URL_to_ping log_file repetitions");
+                    location = args[0];
+                    path = args[1];
+                    break;
+                case 3:
+                    location = args[0];
+                    path = args[1];
+                    repetitions = Convert.ToInt32(args[2]);
+                    break;
             }
-            else {
-                string path = args[1];
-                for (int i = 0; i < 450; i++) {
-                    //Prepare the log message, and the request
-                    string text = Environment.NewLine + DateTime.Now;
-                    WebRequest request = WebRequest.Create(args[0]);
 
-                    //See if the response was OK
-                    try {
-                        using (WebResponse response = request.GetResponse()) {
-                            text = text + " : Success";
-                        }
-                    }
-                    //If not, catch the exception, and state what the status code returned was
-                    catch (WebException ex) {
-                        using (WebResponse response = ex.Response) {
-                            HttpWebResponse httpResponse = (HttpWebResponse)response;
-                            text = text + " : Failed. Error: " + (string)httpResponse.StatusCode.ToString();
-                        }
-                    }
-                    //Write this to file, and to the console.
-                    File.AppendAllText(path, text);
-                    Console.WriteLine(text);
+            //Write a message to begin the file
+            WriteAndLog(Environment.NewLine + $"Pinging {location} and logging to {path}");
 
-                    //Wait 1 minute before going again
-                    Thread.Sleep(60000);
+            //Run this every minute for specified number of minutes (default 1)
+            for (int i = 0; i <= repetitions; i++) {
+
+                //If this is not the first run...
+                if (i != 0) {
+                    //Then wait 1 minute before next ping
+                    if (i != repetitions) {
+                        Thread.Sleep(1000 * 60);
+                    }
+                    //But if this is the last run, stop now
+                    else {
+                        break;
+                    }
                 }
+
+                //Prepare the log message, and the request
+                string text = Environment.NewLine + DateTime.Now;
+                WebRequest request = WebRequest.Create(location);
+
+                //See if the response was OK
+                try {
+                    using (WebResponse response = request.GetResponse()) {
+                        text = text + " : Success";
+                    }
+                }
+                //If not, catch the exception, and state what the status code returned was
+                catch (WebException ex) {
+                    using (WebResponse response = ex.Response) {
+                        HttpWebResponse httpResponse = (HttpWebResponse)response;
+                        text = text + " : Failed. Error: " + (string)httpResponse.StatusCode.ToString();
+                    }
+                }
+                
+                WriteAndLog(text);
+
+
             }
-            
+        }
+        //Write a specified message to specified file, and to the console.
+        static void WriteAndLog(string message) {
+            File.AppendAllText(path, message);
+            Console.WriteLine(message);
         }
     }
+
 }
